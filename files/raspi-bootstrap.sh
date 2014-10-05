649146861bdda468d7fb20f045a3a4299431b689
Alex White <VVu@geekfarm.org>
2014-10-05 12:19:57 -0700

    install latest version of puppet
    i finally upgraded my puppetmaster.

--- files/raspi-bootstrap.sh
+++ files/raspi-bootstrap.sh
@@ -35,7 +35,7 @@ echo "Y" | sudo apt-get install ruby-dev                  || exit 1
 
 echo
 echo installing puppet
-sudo gem install --no-ri --no-rdoc --version 3.2.1 puppet || exit 1
+sudo gem install --no-ri --no-rdoc puppet || exit 1
 
 echo
 echo installing librarian-puppet
